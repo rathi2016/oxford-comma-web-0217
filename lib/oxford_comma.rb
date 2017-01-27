d68ed62b6e645576c59082e7a12a4e2e71d8471d
Rathi Nair-Das <rathind14@gmail.com>
2017-01-27 09:40:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+arr1 = [ ]
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+
+    array.each.with_index do |ele,idx|
+      if idx == array.length-1
+        arr1 << "and #{ele}"
+      else
+      arr1 << "#{ele},"
+    end
+    end
+    arr1.join(" ")
+
+
+  end
+
 
 end
